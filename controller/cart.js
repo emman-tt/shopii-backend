ab39b50e7bc762e195f243238393e3761b3e2181
Your Name <Emmanuelaquarius2006@gmail.com>
2025-12-11 01:27:24 +0000

    Implement UpdateCart and DeleteFromCart functions; update routes for cart management

--- controller/cart.js
+++ controller/cart.js
@@ -5,6 +5,69 @@ import { UserModel } from '../database/user.js'
 import { CartModel } from '../database/cart.js'
 import { cartProduct } from '../database/cartProducts.js'
 
+export async function UpdateCart (req, res) {
+  try {
+    const { productID, qty } = req.query
+
+    const prodID = Number(productID)
+    const quantity = Number(qty)
+
+    const currentUser = await UserModel.findByPk(1)
+    const cart = await currentUser.getCart()
+    const item = await cart.getProducts({
+      where: {
+        id: productID
+      }
+    })
+
+    if (item && item.length > 0) {
+      const product = item[0]
+      const oldQuantity = product.cartProduct.quantity
+      const newQuantity = oldQuantity + quantity
+
+      await product.cartProduct.update({ quantity: newQuantity })
+
+      console.log('old :', oldQuantity)
+      console.log('new :', newQuantity)
+
+      return res.status(200).json({
+        id: prodID,
+        quantity: newQuantity
+      })
+    }
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({
+      err: error.message
+    })
+  }
+}
+
+export async function DeleteFromCart (req, res) {
+  try {
+    const { productID } = req.query
+    const user = await UserModel.findByPk(1)
+    const cart = await user.getCart()
+
+    if (cart && user) {
+      await cartProduct.destroy({
+        where: {
+          cartId: cart.id,
+          productId: productID
+        }
+      })
+    }
+
+    res.status(200).json({
+      msg: 'Deleted succesfully'
+    })
+  } catch (error) {
+    res.status(500).json({
+      err: error.message
+    })
+  }
+}
+
 export async function SaveToCart (req, res) {
   try {
     const currentUser = await UserModel.findByPk(1)
@@ -69,12 +132,11 @@ export async function fetchCart (req, res) {
     })
 
     if (items) {
-    const products=  items.map(item => item.dataValues)
+      const products = items.map(item => item.dataValues)
       return res.status(200).json({
         products: products
       })
     }
-
   } catch (error) {
     console.log(error.message)
     return res.status(500).json({
--- routes/route.js
+++ routes/route.js
@@ -2,16 +2,22 @@ import express from 'express'
 const router = express.Router()
 import upload from '../config/cloudinary.js'
 import { Uploader } from '../controller/Upload.js'
-import { GetAllProducts} from '../controller/Product.js'
+import { GetAllProducts } from '../controller/Product.js'
 import { SaveToCart } from '../controller/cart.js'
 import { GetSPP } from '../controller/Product.js'
-import { readTotal } from '../controller/cart.js'
-import { fetchCart } from '../controller/cart.js'
+import {
+  readTotal,
+  DeleteFromCart,
+  fetchCart,
+  UpdateCart
+} from '../controller/cart.js'
 router.get('/getSPP', GetSPP)
 router.post('/upload', upload.single('image'), Uploader)
 router.get('/AllProducts', GetAllProducts)
 router.post('/cart', SaveToCart)
 router.get('/fetchCart', fetchCart)
 router.put('/fetchTotal', readTotal)
+router.delete('/RemoveCart', DeleteFromCart)
+router.put('/UpdateCart', UpdateCart)
 
 export default router
