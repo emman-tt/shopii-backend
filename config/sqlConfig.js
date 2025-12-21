5e5d66cb30e6fac360c3673f72aa98b7ffd47e40
Your Name <Emmanuelaquarius2006@gmail.com>
2025-12-21 23:08:59 +0000

    solving the sql problem in prod

--- config/sqlConfig.js
+++ config/sqlConfig.js
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import { Sequelize } from 'sequelize'
-export const sequelize = new Sequelize('shopii', 'root', 'Trapper911$', {
-  dialect: 'mysql',
-  host: 'localhost'
-}) 
-// export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+// export const sequelize = new Sequelize('shopii', 'root', 'Trapper911$', {
 //   dialect: 'mysql',
-//   protocol: 'mysql',
-//   logging: false
-// })
+//   host: 'localhost'
+// }) 
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  protocol: 'mysql',
+  logging: false
+})
